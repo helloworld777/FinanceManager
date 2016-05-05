42a66e393adeabc2699be1f60ad0c4c8197675ce
lenovo <15386369307@163.com>
2016-05-05 15:01:17 +0800

    add jsonutil

---
+++ app/src/main/java/com/lu/momeymanager/util/JsonUtil.java
@@ -0,0 +1,40 @@
+package com.lu.momeymanager.util;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lenovo on 2016/5/5.
+ */
+public class JsonUtil {
+
+    public static <T> List<T> getBeansFromJson(Class<?> clazz, String jsons){
+        String[] json=jsons.replace("[", "").replace("]", "").split("},");
+        List<T> ts=new ArrayList<T>();
+        for(String j:json){
+            T t=getBeanFromJson(clazz,j);
+            ts.add(t);
+        }
+        return ts;
+    }
+    public static <T> T getBeanFromJson(Class<?> clazz,String json){
+        T t=null;
+        try {
+            t = (T) clazz.newInstance();
+            String[] fields=json.replace("\"", "").replace("{","").replace("}", "").trim().split(",");
+            for(String field:fields){
+                String[] values=field.split(":");
+                Field f=clazz.getDeclaredField(values[0]);
+                if(f!=null){
+                    f.setAccessible(true);
+                    f.set(t, values[1]);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return t;
+    }
+}
